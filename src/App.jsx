39376dc0a80929e70c480942ee34829a2c6d6c85
eucindyn <mcindynascimento@gmail.com>
2023-03-29 22:32:54 -0300

    :boom: fix: alterar tipo de importação de fonte

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import IconLogo from "./components/IconLogo";
 
 const App = () => {
-  return <IconLogo />;
+  return (
+    <>
+      <IconLogo />
+      <h1>helloooooo</h1>
+    </>
+  );
 };
 
 export default App;
--- src/GlobalStyles/Elements/Base/index.js
+++ src/GlobalStyles/Elements/Base/index.js
@@ -1,6 +1,33 @@
 import { createGlobalStyle } from "styled-components";
 
 const Base = createGlobalStyle`
+  @font-face {
+    font-family: 'mago3';
+    src: url('fonts/mago3.woff2') format('woff2'),
+        url('fonts/mago3.woff') format('woff');
+    font-weight: 500;
+    font-style: normal;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'mago1';
+    src: url('fonts/mago1.woff2') format('woff2'),
+        url('fonts/mago1.woff') format('woff');
+    font-weight: 500;
+    font-style: normal;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'mago2';
+    src: url('fonts/mago2.woff2') format('woff2'),
+        url('fonts/mago2.woff') format('woff');
+    font-weight: 500;
+    font-style: normal;
+    font-display: swap;
+}
+
   body {
     background-color: var(--color-bg);
     font-family: "mago3", sans-serif;
