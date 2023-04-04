e07d5b60630f5cd87ec2f9c9c8a0e1adfddb6acb
eucindyn <mcindynascimento@gmail.com>
2023-04-04 16:13:31 -0300

    :lipstick: feat: estilização css do componente

--- src/components/Logo/Logo.styles.js
+++ src/components/Logo/Logo.styles.js
@@ -2,7 +2,11 @@ import styled from "styled-components";
 
 const Name = styled.figcaption``;
 
-const Logo = styled.figure``;
+const Logo = styled.figure`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
 
 export default {
   Logo,
