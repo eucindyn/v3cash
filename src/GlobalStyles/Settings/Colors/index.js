1046b46a0d1f7b2d158dd9b15a3944363076f6b0
eucindyn <mcindynascimento@gmail.com>
2023-04-05 11:50:41 -0300

    :lipstick: feat: adicionar sombra ao texto

--- src/GlobalStyles/Settings/Colors/index.js
+++ src/GlobalStyles/Settings/Colors/index.js
@@ -3,8 +3,9 @@ import { createGlobalStyle } from "styled-components";
 const Colors = createGlobalStyle`
  :root {
   --color-bg: #A4C263;
-  --color-txt: #C87C86;
   --color-border: #664165;
+  --color-border2: #C49A6C;
+  --color-txt: #C87C86;
   --color-btn: #E8CFA6;
  }
 `;
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -5,8 +5,16 @@ const Button = styled.button`
   border: none;
   font-family: inherit;
   font-size: 1.2rem;
-  letter-spacing: 0.1em;
+  letter-spacing: 0.2rem;
   text-transform: uppercase;
+  color: var(--color-border);
+  text-shadow: 0.08rem 0.08rem 0 var(--color-border2),  
+               0.08rem -0.08rem 0 var(--color-border2),
+               -0.08rem 0.08rem 0 var(--color-border2),
+               -0.08rem -0.08rem 0 var(--color-border2),
+               0.08rem 0rem 0 var(--color-border2),
+               -0.08rem 0rem 0 var(--color-border2);
+  padding: 1rem 0.8rem;             
 `
 
 export default Button;
